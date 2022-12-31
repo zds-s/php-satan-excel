9270757941d1dab59788ce7be4d30d50c4cade07
ShuSRun <2771717608@qq.com>
2022-12-31 14:34:36 +0800

    Fix: - 更新导出示例 - 更新README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # excel 导入导出
+
+# 安装
+
+## composer
+
+```shell
+composer require death_satan/death_satan/satan-excel -vvv
+```
+
+#使用
+
+- 导出示例`example/array_export.php`
--- example/array_export.php
+++ example/array_export.php
@@ -18,6 +18,7 @@ $data = [
         'pass'=>'123456'
     ]
 ];
+$dataFields = ['id','name','pass'];
 $filedTitles = [
     '序号','名称','密码'
 ];
@@ -28,17 +29,26 @@ $arrayExportConfig->setFirstFields($filedTitles);
 
 $arrayExportConfig->addEvent(ArrayExportConfig::EVENT_SAVING,
     function (Spreadsheet $spreadsheet){
-    // 保存前回调
+    // 保存前回调 在这里对要导出的文件 SpreadSheet进行操作
 //        dump($spreadsheet);
     }
 );
 $arrayExportConfig->addEvent(ArrayExportConfig::EVENT_CELL_FORMAT,
-function (\PhpOffice\PhpSpreadsheet\Cell\Cell $cell,array $raw){
-//    dump($cell->getValue());
+function (\PhpOffice\PhpSpreadsheet\Cell\Cell $cell,array $raw)use ($dataFields,$filedTitles){
+    // 获取当前单元格值
+     $cellValue = $cell->getValue();
+     // 获取当前一列所属的下标
+     $index = array_search($cellValue,$raw);
+     $dataFieldValue = $dataFields[$index];
+     $filedValue = $filedTitles[$index];
+     echo sprintf('当前设置的值:%s,列名:%s,列标题:%s',$cellValue,$dataFieldValue,$filedValue)."\n";
 }
 );
 $export = new ArrayExport($arrayExportConfig);
 
+# 保存为文件
 $file = $export->save();
+# 获取保存
 $file_path = $file->getRealPath();
+// 将文件移动到当前目录下并且重命名为 demo.xlsx
 copy($file_path,__DIR__.DIRECTORY_SEPARATOR.'demo.xlsx');
