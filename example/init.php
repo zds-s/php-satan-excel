25575bfcdcfb08aec287b8a5e3067a5a5d62bfd9
DeathSatan <death-satan@qq.com>
2023-08-01 09:25:04 +0800

    fix: - add example

---
+++ example/init.php
@@ -0,0 +1,25 @@
+<?php
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+function includeAll(?string $dirs_path = null)
+{
+    $notIncludes = [
+        'read.php','writer.php','.','..'
+    ];
+    $dirs_path = $dirs_path ?? __DIR__;
+    foreach (scandir($dirs_path) as $dir) {
+        if (in_array($dir,$notIncludes))
+        {
+            continue;
+        }
+        $pathFile = $dirs_path.DIRECTORY_SEPARATOR.$dir;
+        if (is_dir($pathFile)){
+            includeAll($pathFile);
+            continue;
+        }
+        $pathinfo = pathinfo($pathFile);
+        if ($pathinfo['extension'] === 'php'){
+            require_once $pathFile;
+        }
+    }
+}
+includeAll();
